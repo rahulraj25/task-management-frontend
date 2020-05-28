111eaaf8467e98277479f66c3ffffc1c38d8c240
Gaurav Kumar <gauravsingh3525@gmail.com>
2020-05-29 00:14:08 +0530

    merged

--- src/App.js
+++ src/App.js
@@ -4,16 +4,12 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/layout/Header";
 import Dashboard from "./components/Dashboard";
-import Rahul from "./components/layout/Rahul";
-import Gaurav from "./components/layout/gaurav";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Dashboard />
-      <Rahul />
-      <Gaurav />
     </div>
   );
 }
