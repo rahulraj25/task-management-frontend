20875e5418e806d09970a8097034f0bc57babc4f
Gaurav Kumar <gauravsingh3525@gmail.com>
2020-06-08 23:49:33 +0530

    added due date logic

--- src/components/task/TaskCard.js
+++ src/components/task/TaskCard.js
@@ -5,8 +5,15 @@ import { deleteTask } from "../../actions/taskAction";
 import PropTypes from "prop-types";
 import { confirmAlert } from "react-confirm-alert";
 import "react-confirm-alert/src/react-confirm-alert.css";
+import moment from "moment";
 
 class TaskCard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dateDiff: "",
+    };
+  }
   onDeleteClick = (id) => {
     confirmAlert({
       title: "Confirm",
@@ -25,6 +32,11 @@ class TaskCard extends Component {
 
   render() {
     const { task } = this.props;
+    const present = moment();
+    console.log("Due Date: " + this.props.task.dueDate);
+    const end = moment(this.props.task.dueDate);
+    const diff = end.diff(present, "days");
+    console.log("Date difference: " + diff);
     return (
       <div className="card mb-1 bg-light">
         <div className="closebuttonpanel">
@@ -47,7 +59,7 @@ class TaskCard extends Component {
         </div>
         <div className="card-body bg-light">
           <h4 className="card-text">{task.description}</h4>
-          <p className="card-text">Due in _____ Days</p>
+          <p className="card-text">Due in {diff} Days</p>
         </div>
         <Link to={`/updateTask/${task.id}`} className="btn btn-primary">
           View / Update
